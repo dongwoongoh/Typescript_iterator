98dff3d0218945be239b52a0ed0040cca20fa315
MAD <74044653+maaaaaaaaad@users.noreply.github.com>
2023-04-14 08:58:09 +0000

    e

--- main.go
+++ main.go
@@ -4,6 +4,28 @@ import (
 	"fmt"
 )
 
+func generateCombinations(arr []int, r int) [][]int {
+	if r == 0 {
+		return [][]int{[]int{}}
+	}
+
+	if len(arr) == 0 {
+		return [][]int{}
+	}
+
+	head := arr[0]
+	tail := arr[1:]
+
+	withoutHead := generateCombinations(tail, r)
+	withHead := generateCombinations(tail, r-1)
+
+	for i := 0; i < len(withHead); i++ {
+		withHead[i] = append([]int{head}, withHead[i]...)
+	}
+
+	return append(withHead, withoutHead...)
+}
+
 func main() {
 	fmt.Println("Hello, World!")
 }
--- src/02_generator.ts
+++
@@ -1,6 +0,0 @@
-export function* generator() {
-  console.log("Strat...🕐");
-  let value = 1;
-  while (value < 4) yield value++;
-  console.log("Finished!");
-}
