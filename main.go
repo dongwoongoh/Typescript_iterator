3f69c88a8c362b0ea2df2a325d714a54e3131ebc
maaaaaaaaad <74044653+maaaaaaaaad@users.noreply.github.com>
2023-04-19 07:45:28 +0000

    e

--- main.go
+++ main.go
@@ -36,8 +36,9 @@ func main() {
 	}
 
 	discount := float64(naturalNum) * (discountRate / 100)
-	result := int(discount)
-	fmt.Printf("할인 금액은 %d 입니다.", result)
+	payment := float64(naturalNum) - discount
+	result := int(payment)
+	fmt.Printf("최종 지불 금액은 %d 입니다.", result)
 }
 
 func getUserInput() string {
